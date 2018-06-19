a74c54bf343640a9180af632a8031d6b6095eeb2
Tom <tom@agtoft.com>
2018-06-19 20:57:17 +1000

    First

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # f5_azure_tut
+
+F5 Azure template and Application for instruction on F5 VE
+
+## Requires
+- Azure CLI
+- Default Tenancy and Subscription set
+
+## Deploy
+./build.sh
+
---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+env=$1
+
+if [ "${env}" == "" ]; then
+    echo "No environment provided"
+fi
+
+if [ ! "${env}" ~= /^[A-Za-z0-9]+$/ ]; then
+    echo "Environment only accepts alphanumeric"
+fi
+
+
+rsg_name="rg-tomtec-f5-tut-${env}"
+deployment_name="${rsg_name}-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)"
+template_file_name="arm_template.json"
+parameter_file_name="parameters.json"
+
+location_long="Australia SouthEast"
+
+az login
+
+ret=$(az group show --name "${rsg_name}" | wc -l)
+if [ $ret -ne 0 ]; then
+    echo "Azure group ${rsg_name} exists; skipping creation."
+else
+    echo "Azure group ${rsg_name} needs creating."
+    az group create --name "${rsg_name}" --location "${location_long}"
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+fi
+
+az group deployment create \
+    --name "${deployment_name}" \
+    --mode "incremental" \
+    --resource-group "${rsg_name}" \
+    --template-file "${template_file_name}" \
+    --paramaters-file @"${parameter_file_name}"
+
+az logout
