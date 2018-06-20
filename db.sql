1204a180b3499ca975e05bcd30dcc67ba6da25c9
Tom <tom@agtoft.com>
2018-06-20 15:29:45 +1000

    fix table insert

--- db.sql
+++ db.sql
@@ -2,7 +2,7 @@ create database tomtec;
 use tomtec;
 create table user (id int PRIMARY KEY AUTO_INCREMENT, user varchar(255), pass varchar(255), fullname varchar(255));
 
-insert into table user (user, pass, fullname) values ('admin', 'pass', 'My Administrator');
-insert into table user (user, pass, fullname) values ('tom', 'pass', 'Me');
-insert into table user (user, pass, fullname) values ('bob', 'pass', 'Da Boss');
-insert into table user (user, pass, fullname) values ('troy', 'pass', 'Mini Boss');
+insert into user (user, pass, fullname) values ('admin', 'pass', 'My Administrator');
+insert into user (user, pass, fullname) values ('tom', 'pass', 'Me');
+insert into user (user, pass, fullname) values ('bob', 'pass', 'Da Boss');
+insert into user (user, pass, fullname) values ('troy', 'pass', 'Mini Boss');
