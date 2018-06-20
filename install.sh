651f988ee12109388dcce84d647990e391046ad8
Tom <tom@agtoft.com>
2018-06-20 14:20:27 +1000

    .

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
-yum -y install mysql npm nodejs
+yum -y install mysql npm nodejs jq
 
 get_ip() {
+    ip_list=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/network?api-version=2017-08-01" | grep 'privateIpAddress' |awk '{print $2}' | sed -e 's/\"//g' -e 's/,//g')
 
+    echo $ip_list
 }
+
+ips=$(get_ip)
+ip1=$(echo $ips | awk '{print $1}')
+ip2=$(echo $ips | awk '{print $2}')
 
 mysql < db.sql
 
